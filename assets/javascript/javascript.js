89fff2717c0fa8e680fd2a7ff3942cb08e7f2d5c
afletch24 <anfletcher24@gmail.com>
2017-08-29 13:17:27 -0500

    responsive styling

--- assets/javascript/javascript.js
+++ assets/javascript/javascript.js
@@ -17,7 +17,7 @@ var sendRequest = function(){
 
     $.ajax({
         method: "POST",
-        url: "https://www.googleapis.com/qpxExpress/v1/trips/search?key=AIzaSyA1K7p4y-7d3r4-Cj_QcAnpZRfnZKWnho0",
+        url: "https://www.googleapis.com/qpxExpress/v1/trips/search?key=AIzaSyAMl5As1sJ8De-76Dt8QgvlIursOApZ38o",
         contentType: 'application/json', 
         dataType: 'json',
         data: JSON.stringify(FlightRequest)
